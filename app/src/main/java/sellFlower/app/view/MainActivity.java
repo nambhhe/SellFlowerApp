114711cfbaba2d0ae129db73e8bccb42962c3a98
nambhhe <nambhhe176453@fpt.edu.vn>
2024-09-26 02:17:22 +0700

    Initial Commit

--- app/src/main/java/sellFlower/app/view/MainActivity.java
+++ app/src/main/java/sellFlower/app/view/MainActivity.java
@@ -26,24 +26,7 @@ public class MainActivity extends AppCompatActivity {
         SellFlowerDatabase db = DatabaseInstance.getInstance(this);
         userDao = db.userDao();
 
-        // Example operations
-        insertData();
-        queryData();
-    }
-    private void insertData() {
-        // Insert a new user
-        User newUser = new User("John Doe", "john@example.com");
-        userDao.insertUser(newUser);
-
     }
 
-    private void queryData() {
-        // Query all users
-        ArrayList<User> users = userDao.getAllUsers();
-        for (User user : users) {
-            Log.d("User", "ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
-        }
-
-    }
 
 }
